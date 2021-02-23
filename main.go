c0470af883eec2890cc1811cb8305178d90a22a0
dkaiser014 <dkaiser014@protonmail.com>
2021-02-23 20:12:29 +0100

    modified: added router to main.go

--- main.go
+++ main.go
@@ -1,52 +1,19 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"valgus/pkg/router"
 )
 
 // TODO's
-// > First thing i want to do is running a http server using go `net/http` package.
-// > Then I want to be able to render and .html file.
-// > Once I'm able to render .html files I want to make a Router to handle all the request.
+// > First thing i want to do is running a http server using go `net/http` package. (Done)
+// > Then I want to be able to render and .html file. (Done)
+// > Once I'm able to render .html files I want to make a Router to handle all the request. (Done)
 // > After that I would like to implement error handling for non-existing pages.
 // > To finish off it will be nice to use Pug and Sass as transpilers.
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w, "index", p)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w, "view", p)
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/view/", viewHandler)
+	router.Router()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

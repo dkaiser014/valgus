ef466b834572239ca158e80781744c752bef6efd
dkaiser014 <dkaiser014@protonmail.com>
2021-02-23 20:13:00 +0100

    add: added handlers.go to handle the webpages

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,46 +3,42 @@ package handlers
 import (
 	"html/template"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
 )
 
-// Page represents a structure for the content
-// of the website
+// Page represents the structure of the file
 type Page struct {
 	Title string
 	Body  []byte
 }
 
-// LoadPage reads the file and returns the title and
-// the body(content) of the file
+// LoadPage returns the title and the body(content)
+// of a specific file
 func LoadPage(title string) (*Page, error) {
 	filename := title
 	body, err := ioutil.ReadFile(filename)
-
 	if err != nil {
-		log.Fatal("Errored:", err)
 		return nil, err
 	}
-
 	return &Page{Title: title, Body: body}, nil
 }
 
-// RenderTemplate parses the file and writes the
-// output to a template
+// RenderTemplate parses the contents of a file
+// and renders it to a template
 func RenderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
+	t, _ := template.ParseFiles("web/views" + string(os.PathSeparator) + tmpl + ".html")
 	t.Execute(w, p)
 }
 
-// IndexHandler loads a page based on a URL
+// IndexHandler handles the index route
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/"):]
 	p, _ := LoadPage(title)
 	RenderTemplate(w, "index", p)
 }
 
-// AboutHandler loads a page based on a URL
+// AboutHandler handles the about route
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/about/"):]
 	p, _ := LoadPage(title)

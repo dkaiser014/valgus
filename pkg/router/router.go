1c4d0e90277c966eb087c8c157b8a4bf76b9242c
dkaiser014 <dkaiser014@protonmail.com>
2021-03-02 23:02:12 +0100

    update: added static route for assets

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -9,4 +9,9 @@ import (
 func Router() {
 	http.HandleFunc("/", handlers.IndexHandler)
 	http.HandleFunc("/about/", handlers.AboutHandler)
+
+	// Static asssets
+	fs := http.FileServer(http.Dir("web/assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/about/static/", http.StripPrefix("/about/static/", fs))
 }
